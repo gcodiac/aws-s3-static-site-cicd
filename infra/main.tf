a96fa51f0e9195e60b7f2eee6d080c47bf461418
Ali Albakri <m.vy@hotmail.com>
2026-09-20 19:55:03 +0100

    feat(terraform): create a public S3 website bucket

---
+++ infra/main.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "site" {
+  bucket = var.bucket_name
+}
+
+# New buckets block public access. This site is meant to be public, so switch it off.
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+# Let anyone read the objects.
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket     = aws_s3_bucket.site.id
+  depends_on = [aws_s3_bucket_public_access_block.site]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.site.arn}/*"
+    }]
+  })
+}
